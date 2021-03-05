a7296867ee4faf264eb86a1ad50bbc9933acb10e
kevin.sasso <kevin.sasso@medicamentos.gob.sv>
2021-03-04 21:13:48 -0600

    :sparkles: Nuevas funcionalidad principales

---
+++ proyecto-ped/arcocrud.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyecto_ped
+{
+    public partial class arcocrud : Form
+    {
+        public bool control; // variable de control
+        public int dato; // el dato que almacenará el vértice
+        public arcocrud()
+        {
+            InitializeComponent();
+            control = false;
+            dato = 0;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dato = Convert.ToInt16(textBox1.Text.Trim());
+
+                if (dato < 0)
+                {
+                    MessageBox.Show("Debes ingresar un valor positivo", "Error", MessageBoxButtons.OK,
+                MessageBoxIcon.Exclamation);
+                }
+                else
+                {
+                    control = true;
+                    Hide();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Debes ingresar un valor numerico");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            control = false;
+            Hide();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void arcocrud_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(null, null);
+            }
+        }
+    }
+}
---
+++ proyecto-ped/verticecrud.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyecto_ped
+{
+    public partial class verticecrud : Form
+    {
+        public bool control; // variable de control
+        public string dato; // el dato que almacenará el vértice
+        public verticecrud()
+        {
+            InitializeComponent();
+            control = false;
+            dato = " ";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dato = textBox1.Text.Trim();
+            if ((dato == "") || (dato == " "))
+            {
+                MessageBox.Show("Debes ingresar un valor", "Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+            }
+            else if (dato.Length > 2)
+            {
+                var result = MessageBox.Show("Se tomaran en cuenta solamente los dos primeros carateres. Desea continuar?", "Advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    dato = dato.Substring(0, 2);
+                    control = true;
+                    Hide();
+                }
+            }
+            else
+            {
+                control = true;
+                Hide();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            control = false;
+            Hide();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(null, null);
+            }
+        }
+    }
+}
